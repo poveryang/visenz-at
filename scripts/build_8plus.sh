2fdaff8ac94a595a197ccbcb7a4f50c847e79895
yjunj <junjie.yang@smartmore.com>
2022-10-17 21:07:09 +0800

    [Feat]: Modified build script

--- scripts/build_8plus.sh
+++ scripts/build_8plus.sh
@@ -9,7 +9,8 @@ cd build_8plus
 -DCMAKE_BUILD_TYPE=Release \
 -DCMAKE_C_COMPILER=/opt/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-gcc \
 -DCMAKE_CXX_COMPILER=/opt/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-g++ \
--DTARGET_PLATFORM=imx8plus \
+-DTARGET_PLATFORM="imx8plus" \
+-DCMAKE_INSTALL_PREFIX="../release/imx8plus" \
 -S .. \
 -B .
 
