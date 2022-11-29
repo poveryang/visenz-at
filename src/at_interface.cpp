a53ab7b08f000c282a58559da4ff6d4890c9829f
yjunj <junjie.yang@smartmore.com>
2022-11-29 16:57:52 +0800

    [Feat]：Modified Camera config(MIN_ET)

--- src/at_interface.cpp
+++ src/at_interface.cpp
@@ -284,8 +284,8 @@ namespace at {
         // Engineering Version Number
         #define TRIA_VERSION_E_MAJOR 3
         #define TRIA_VERSION_E_MINOR 3
-        #define TRIA_VERSION_E_PATCH 3
-        #define TRIA_VERSION_E_RC    4
+        #define TRIA_VERSION_E_PATCH 4
+        #define TRIA_VERSION_E_RC    1
 
         #define AUX_STR_EXP(__A) #__A
         #define AUX_STR(__A) AUX_STR_EXP(__A)
