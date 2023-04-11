da72a66c19c5c758505f048c56425e51e130100d
yjunjie <junjie.yang@smartmore.com>
2023-04-11 15:03:56 +0800

    feat: Update script of run on Imx8plus

--- scripts/VS_run_8plus.sh
+++ scripts/VS_run_8plus.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
-#!/usr/bin/bash
 
 # Set remote device ip
 DEV_IP=192.168.8.21
 
-# Clean local files
-SAVE_DIR=${HOME}/Downloads/AT_RES_VS1000P/$(date +%m%d%H%M)
-echo "$SAVE_DIR" && mkdir "$SAVE_DIR" -p
+# Set environment
+target_platform="imx8plus"
+script_dir=$(cd "$(dirname "$0")" || exit; pwd)
+root_dir=$(cd "$script_dir/.." || exit; pwd)
+build_dir=$root_dir/build-$target_platform
+save_dir=$HOME/Downloads/AT-test-"$target_platform"/$(date +%m%d%H%M%S)
+
+if [ ! -d "$save_dir" ]; then
+mkdir -p "$save_dir"  && echo "build folder is created"
+fi
 
 # Scp execute file and lib to remote device
 scp -O ./release/VS/imx8plus/lib/libAT.so root@$DEV_IP:/tmp
 scp -O ./release/VS/imx8plus/bin/AT_TEST root@$DEV_IP:/tmp
-scp -O ./modules/hmap-generator/model/hmap-uint8-v7k-kl.tmfile root@$DEV_IP:/tmp
-scp -O ./modules/hmap-generator/model/hmap-v3-e99-fp32.tmfile root@$DEV_IP:/tmp
+scp -O ./modules/hmap-generator/model/hmap-v4-e200-uint8.tmfile root@$DEV_IP:/tmp
 
 # Connect to remote device(Scanner)
 ssh -tt root@$DEV_IP << remotessh
 killall SmoreScanner
-killall SmorescannerUdpProcess
-killall SmoreScannerUpgrade
 export LD_LIBRARY_PATH=/tmp:$LD_LIBRARY_PATH
 
 mkdir /tmp/at_res
@@ -28,7 +31,7 @@ exit
 remotessh
 
 # Transfer images to local
-scp -O -q root@$DEV_IP:/tmp/at_res/\* $SAVE_DIR
+scp -O -q root@$DEV_IP:/tmp/at_res/* $save_dir
 
 # Clean remote files
 ssh -tt root@$DEV_IP << remotessh
