45231ef4f87862122341d441e275215b1e9540c9
yjunj-mac <yjunjie@163.com>
2025-03-10 16:19:14 +0800

    feat: add Docker build scripts for imx8mini, imx8plus, nova, and rknn platforms

---
+++ scripts/build_imx8plus_in_docker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Get the directory of target project
+script_dir=$(dirname "$0")
+project_dir=$(realpath "$script_dir/..")
+echo "Project directory: $project_dir"
+
+# Clear the build directory
+rm -rf "$project_dir/build/imx8plus"
+
+# Build the docker image
+container_id=$(docker run -it \
+    --platform linux/amd64 \
+    -d \
+    -v "$project_dir":/workspace \
+    compiler:imx8plus)
+
+# Build the project in the docker container
+docker exec "$container_id" bash -c "
+    /usr/bin/cmake \
+        -DPLATFORM='imx8plus' \
+        -DCMAKE_C_COMPILER='/opt/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-gcc' \
+        -DCMAKE_CXX_COMPILER='/opt/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-g++' \
+        -S /workspace \
+        -B /workspace/build/imx8plus &&
+    /usr/bin/cmake \
+        --build /workspace/build/imx8plus \
+        --target install \
+        -- -j 16
+"
+
+# Stop and remove the docker container
+docker stop "$container_id"
+docker rm "$container_id"
